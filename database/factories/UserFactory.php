bca741442d8fef96e05b05c66c959b691ec229a4
Rengga <renggarendy22@gmail.com>
2024-10-02 22:43:02 +0700

    Model Factories

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,8 +28,12 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            // $a = $a ? $a : $b; // ternary operator
+            // $a = $a ?: $b; // elvis operator
+            // $a ??= $b; //null coalescing operator
             'remember_token' => Str::random(10),
         ];
+        // App\Models\User::factory(10)->create(); 
     }
 
     /**
@@ -37,8 +41,18 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
+        // App\Models\User::factory(5)->unverified()->create(); 
+
     }
+
+    // public function admin(): static
+    // {
+    //     return $this->state(fn(array $attributes) => [
+    //         'is_admin' => true,
+    //     ]);
+    //     // App\Models\User::factory(3)->admin()->create();
+    // }
 }
