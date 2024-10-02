dbb347c86f7659b4bd37fa7015f64dc6a14e0ab6
Rengga <renggarendy22@gmail.com>
2024-10-02 16:30:38 +0700

    view data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,54 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Rengga Rendi Saputra', "title" => "About Page"]);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ["title" => "Blog"]);
+Route::get('/posts', function () {
+    return view('posts', ["title" => "Blog", 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Rengga',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores obcaecati rem
+            soluta animi architecto
+            ut, quaerat alias, quisquam consequatur, quibusdam quis nam debitis eum est voluptatibus molestias.
+            Inventore, omnis! In?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rendi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam nulla, dolorum aspernatur, temporibus dolor similique ipsam beatae praesentium veniam dolore suscipit obcaecati odit deserunt. Ipsum odio minima aspernatur optio minus.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{id}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Rengga',
+            'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores obcaecati rem
+            soluta animi architecto
+            ut, quaerat alias, quisquam consequatur, quibusdam quis nam debitis eum est voluptatibus molestias.
+            Inventore, omnis! In?'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rendi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quam nulla, dolorum aspernatur, temporibus dolor similique ipsam beatae praesentium veniam dolore suscipit obcaecati odit deserunt. Ipsum odio minima aspernatur optio minus.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
